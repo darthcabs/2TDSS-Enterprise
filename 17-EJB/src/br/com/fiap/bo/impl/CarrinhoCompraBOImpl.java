e1e78c957718c72a97ad40cd84dc70ed5b5ad37c
DAVID DE SOUZA CARDOSO <RM76860@fiap.com.br>
2017-05-10 20:54:32 -0300

    EJB - Stateful

---
+++ 17-EJB/src/br/com/fiap/bo/impl/CarrinhoCompraBOImpl.java
@@ -0,0 +1,17 @@
+package br.com.fiap.bo.impl;
+
+import javax.ejb.Stateful;
+
+import br.com.fiap.bo.CarrinhoCompraBO;
+
+@Stateful
+public class CarrinhoCompraBOImpl implements CarrinhoCompraBO {
+
+	private double totalBitcoin;
+	
+	@Override
+	public double adicionarBitCoin(double bitcoin) {
+		totalBitcoin += bitcoin;
+		return totalBitcoin;
+	}
+}
